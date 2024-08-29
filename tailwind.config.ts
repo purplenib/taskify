9ba18caed373edeed198e24b51594dbedd1919d2
hvrain <hvrain3222@naver.com>
2024-08-29 10:43:57 +0900

    Add: tailwind color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from 'tailwindcss';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { PluginAPI } from 'tailwindcss/types/config';
 
 const config: Config = {
   content: [
@@ -9,13 +11,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        gray: {
+          50: '#FAFAFA',
+          100: '#EEEEEE',
+          200: '#D9D9D9',
+          300: '#9FA6B2',
+          400: '#787486',
+        },
+        black: {
+          500: '#4B4B4B',
+          600: '#333236',
+          700: '#171717',
+        },
+        violet: '#5534DA',
+        'violet-white': '#F1EFFD',
+        red: '#D6173A',
+        green: '#7AC555',
+        purple: '#760DDE',
+        orange: '#FFA500',
+        blue: '#76A5EA',
+        pink: '#E876EA',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities }: PluginAPI) => {
+      addUtilities({});
+    },
+  ],
 };
 export default config;
