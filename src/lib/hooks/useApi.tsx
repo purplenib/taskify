379e3bbe6aa9892e715230c5aa41573dfd9fb371
김영주 <168513336+purplenib@users.noreply.github.com>
2024-09-02 16:30:43 +0900

    TASK-35 [Fix]: POST 요청 시 필요한 데이터 파라미터로 추가

--- src/lib/hooks/useApi.tsx
+++ src/lib/hooks/useApi.tsx
@@ -3,9 +3,10 @@
 import { useEffect, useState } from 'react';
 
 function useApi<T, U>(
-  apiCall: (params: U) => Promise<T>,
+  apiCall: (params: U, data?: any) => Promise<T>,
   requestParams: U,
   initialValue: T
+  requestData?: any // POST 데이터
 ) {
   const [data, setData] = useState<T>(initialValue);
   const [loading, setLoading] = useState<boolean>(true);
@@ -16,7 +17,7 @@ function useApi<T, U>(
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await apiCall(requestParams);
+        const response = await apiCall(requestParams, requestData);
         setData(response);
       } catch (error) {
         console.error('데이터 가져오기 실패:', error);
@@ -27,7 +28,7 @@ function useApi<T, U>(
     };
 
     fetchData();
-  }, [apiCall, paramsString]);
+  }, [apiCall, paramsString, requestData]);
 
   return { data, loading, error: fetchError };
 }
