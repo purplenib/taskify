a35e31958a4e0588d0889051651e14254732a6ba
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-09-03 19:05:18 +0900

    Merge branch 'develop' of https://github.com/codeit-intermediate-project/taskify into TASK-34--MyDashBoardMarkup

--- src/app/mydashboard/page.tsx
+++ src/app/mydashboard/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client';
 
 import InvitedDashBoardList from 'src/components/MyDashBoard/InvitedDashBoardList';
@@ -8,24 +7,12 @@ import { MyDashboardProvider } from '@core/contexts/MyDashboardProvider';
 export default function MyDashBoard() {
   return (
     <MyDashboardProvider>
-=======
-import JoinedDashBoardList from '@/src/components/MyDashBoard/JoinedDashBoardList';
-import InvitedDashBoardList from '@/src/components/MyDashBoard/InvitedDashBoardList';
-
-export default function MyDashBoard() {
-  return (
-    <>
->>>>>>> decc019688b55116340a2e1a34ca20ebfd0f5c7a
       <header>헤더</header>
       <aside>사이드</aside>
       <main className="flex flex-col gap-12 bg-gray-50 px-6 pt-6">
         <JoinedDashBoardList />
         <InvitedDashBoardList />
       </main>
-<<<<<<< HEAD
     </MyDashboardProvider>
-=======
-    </>
->>>>>>> decc019688b55116340a2e1a34ca20ebfd0f5c7a
   );
 }
