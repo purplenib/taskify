474873b11bb6edda9803236ec24eef719c014718
kzlsk <rlaflsk4887@gmail.com>
2024-09-03 18:42:25 +0900

    TASK-70 [Feat] : color picker 구현 - 수정중

---
+++ src/app/dashboard/[dashboardId]/edit/page.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+// eslint-disable-next-line sort-imports
+import EditDashboard from '@/src/components/edit/EditDashboard';
+import InvitationList from '@/src/components/edit/InvitationList';
+import MemberList from '@/src/components/edit/MemberList';
+
+export default function DashBoardEditPage() {
+  return (
+    <div className="flex h-screen flex-col">
+      <header className="h-[60px] w-full bg-white p-4 shadow-md md:h-[70px]">
+        헤더
+      </header>
+      <div className="flex flex-1">
+        <aside className="w-[67px] bg-gray-200 md:block md:w-[160px] xl:w-[300px]">
+          사이드바
+        </aside>
+
+        <main className="flex-1 overflow-y-auto bg-gray-50 p-4">
+          {/* 경로 임시로 지정해둔 것 */}
+          <Link href="/" className="mb-4 flex items-center gap-2">
+            <Image
+              src="/icons/arrow_left.png"
+              alt="돌아가기"
+              width={16}
+              height={16}
+            />
+            <span className="font-lg-16px-medium">돌아가기</span>
+          </Link>
+          <div className="space-y-6">
+            <EditDashboard />
+            <MemberList />
+            <InvitationList />
+            <button
+              type="button"
+              className="flex h-[52px] w-full max-w-md items-center justify-center rounded-lg border border-solid border-gray-200 shadow font-lg-16px-medium md:w-[320px]"
+            >
+              대시보드 삭제하기
+            </button>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/edit/InvitationList.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import Image from 'next/image';
+import Pagination from '@/src/components/edit/Pagination';
+import usePagination from '@/src/lib/hooks/usePaginavigation';
+
+// 테스트용 더미 데이터
+interface Item {
+  id: number;
+  email: string;
+}
+
+const dummyEmailData: Item[] = Array.from({ length: 12 }, (_, i) => ({
+  id: i + 1,
+  email: `user${i + 1}@example.com`,
+}));
+
+export default function InvitationList() {
+  const itemsPerPage = 5;
+  const { currentPage, handlePageChange } = usePagination({
+    totalItems: dummyEmailData.length,
+    itemsPerPage,
+  });
+
+  const startIdx = (currentPage - 1) * itemsPerPage;
+  const currentItems = dummyEmailData.slice(startIdx, startIdx + itemsPerPage);
+
+  return (
+    <div className="max-w-md rounded-md bg-white p-6 shadow md:mx-0 md:max-w-[544px] xl:max-w-[620px]">
+      {/* 모바일 > 초대 내역 + 페이지네이션 / 초대 버튼 X */}
+      <div className="mb-4 flex items-center justify-between">
+        <h2 className="font-xl-20px-bold">초대 내역</h2>
+
+        <div className="flex items-center gap-2 md:hidden">
+          <Pagination
+            currentPage={currentPage}
+            totalItems={dummyEmailData.length}
+            itemsPerPage={itemsPerPage}
+            onPageChange={handlePageChange}
+          />
+        </div>
+
+        {/* md 이상 > 페이지네이션 + 초대 버튼 */}
+        <div className="hidden items-center gap-4 md:flex">
+          <div className="flex items-center gap-2">
+            <Pagination
+              currentPage={currentPage}
+              totalItems={dummyEmailData.length}
+              itemsPerPage={itemsPerPage}
+              onPageChange={handlePageChange}
+            />
+          </div>
+          <button
+            type="button"
+            className="flex h-8 w-[105px] items-center justify-center gap-2 rounded border border-solid bg-violet text-white shadow font-md-14px-medium"
+          >
+            <Image
+              src="/icons/add_box.png"
+              alt="초대하기"
+              width={16}
+              height={16}
+            />
+            초대하기
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <div className="mb-2 text-gray-600 font-lg-16px-regular">이메일</div>
+        {/* 테스트용 */}
+        {currentItems.map(item => (
+          <div key={item.id} className="w-full border-b border-gray-100">
+            <div className="flex w-full items-center justify-between py-2">
+              <div className="flex-1">{item.email}</div>
+              <button
+                type="button"
+                className="flex h-8 w-20 items-center justify-center rounded border border-solid border-gray-200 text-violet font-md-14px-medium"
+              >
+                취소
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
