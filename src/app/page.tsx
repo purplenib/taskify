8c09822aa9e04e0e29ab9cdabeee3d445e7829dc
Minjo Kwawk <iclock1960@naver.com>
2024-09-04 17:14:39 +0900

    test reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-import LandingBottom from '@components/Home/LandingBottom';
-import LandingMain from '@components/Home/LandingMain';
-import LandingTop from '@components/Home/LandingTop';
-import UnAutherHeader from '@components/Common/UnAuthHeader';
-
 export default function Home() {
-  return (
-    <>
-      <UnAutherHeader />
-      <LandingTop />
-      <LandingMain />
-      <LandingBottom />
-    </>
-  );
+  return <div>홈 화면</div>;
 }
