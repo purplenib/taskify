80054547d9e2c089f8dcab7c626fd18e2d9e3993
hvrain <hvrain3222@naver.com>
2024-09-04 14:32:04 +0900

    TASK-86 [Feat]: DashboardLayout의 dashboardid를 useParams로 받게 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 
 import { MantineProvider } from '@mantine/core';
 import localFont from 'next/font/local';
-import { headers } from 'next/headers';
 
 import DashboardLayout from '@components/@shared/Layout/DashboardLayout';
 import DeviceProvider from '@core/contexts/DeviceContext';
@@ -28,18 +27,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const headerResponse = headers();
-  const dashboardid = headerResponse.get('x-dashboardid');
-
   return (
     <html lang="en">
       <body className={`${pretandard.variable}`}>
         <MantineProvider>
           <DeviceProvider>
             <RootProvider>
-              <DashboardLayout dashboardid={dashboardid}>
-                {children}
-              </DashboardLayout>
+              <DashboardLayout>{children}</DashboardLayout>
             </RootProvider>
           </DeviceProvider>
         </MantineProvider>
