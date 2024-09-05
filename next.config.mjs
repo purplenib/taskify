c1acd3cea57a71cc43c108d842057a4848539867
kzlsk <rlaflsk4887@gmail.com>
2024-09-05 21:49:09 +0900

    save

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,17 @@ const nextConfig = {
       },
     ],
   },
+  webpack: config => {
+    // 추가 Webpack 설정
+    config.module.rules.push({
+      test: /\.m?js$/, // .js 또는 .mjs 파일에 대한 설정
+      resolve: {
+        fullySpecified: false, // Node.js와 호환되지 않는 패키지의 문제를 해결
+      },
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
