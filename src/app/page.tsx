3c337c50b854c54362eeb4d9c88ffcef916bffe1
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-09-06 12:19:40 +0900

    TASK-38 [Test]: 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 'use client';
 
-import Link from 'next/link';
-
 export default function Home() {
-  return (
-    <div className="font-3xl-32px-bold">
-      hello
-      <Link href="/mydashboard">나의 대시보드</Link>
-    </div>
-  );
+  return <div className="font-3xl-32px-bold">hello</div>;
 }
