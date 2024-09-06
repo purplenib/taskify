6e6f7e7d8ee644ab8a5f9d6521df432d26495053
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-09-06 12:27:41 +0900

    TASK-38 [Feat]: 대시보드 초대 응답 로직 보완

--- src/components/MyDashBoard/InvitedDashBoard.tsx
+++ src/components/MyDashBoard/InvitedDashBoard.tsx
@@ -1,70 +1,87 @@
 'use client';
 
-import type {
-  InvitationsDto,
-  InvitationsResponseDto,
-} from '@core/dtos/InvitationsDto';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
 import putInvitations from '@core/api/putInvitations';
+import { useMyDashboard } from '@core/contexts/MyDashboardProvider';
 import { useSearch } from '@lib/hooks/useSearch';
+
 import InviteHeader from './UI/InviteHeader';
 import NoDashboard from './UI/NoDashboard';
 import ReturnButton from './UI/ReturnButton';
 import SearchForm from './UI/SearchForm';
-import { useMyDashboard } from '@core/contexts/MyDashboardProvider';
+import PrimaryButton from '@components/@shared/UI/Button/PrimaryButton';
+
+import type {
+  invitationListDto,
+  invitationListResponseDto,
+} from '@core/dtos/invitationListDto';
 
 interface InvitedDashboardProps {
-  invitationsData: InvitationsResponseDto;
+  invitationListData: invitationListResponseDto;
   loading: boolean;
   error: string | null;
-  onAccept: (invitation: InvitationsDto) => void;
 }
 
 export default function InvitedDashboard({
-  invitationsData,
+  invitationListData,
   loading,
   error,
-  onAccept,
 }: InvitedDashboardProps) {
   const { addDashboard } = useMyDashboard();
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [invitations, setInvitations] = useState<InvitationsDto[]>(
-    invitationsData.invitations
-  );
+  const [invitationList, setInvitationList] = useState<invitationListDto[]>([]);
 
+  // invitationListData가 변경될 때 invitationList 상태 업데이트
+  useEffect(() => {
+    if (invitationListData) {
+      setInvitationList(invitationListData.invitationList);
+    }
+  }, [invitationListData]);
+
+  // 대시보드 검색
   const { filteredResults, handleSearch, handleReset } = useSearch(
     {
-      list: invitationsData.invitations,
-      totalCount: invitationsData.invitations.length,
+      list: invitationList,
+      totalCount: invitationList.length,
     },
     setCurrentPage,
-    (invitation: InvitationsDto) => invitation.dashboard.title
+    (invitation: invitationListDto) => invitation.dashboard.title
   );
 
-  const handleAccept = async (invitation: InvitationsDto) => {
+  // 대시보드 초대 수락
+  const handleAccept = async (invitation: invitationListDto) => {
+    setIsProcessing(true);
     try {
       await putInvitations(invitation.id, true);
-
-      // 로컬 대시보드 추가
-      const newDashboard = {
+      const newInvitedDashboard = {
         id: invitation.dashboard.id,
         title: invitation.dashboard.title,
       };
-      addDashboard(newDashboard);
-
-      onAccept(invitation);
-      setInvitations(prev => prev.filter(item => item.id !== invitation.id));
+      addDashboard(newInvitedDashboard); // 로컬에 대시보드 추가
+      setInvitationList(prev => prev.filter(item => item.id !== invitation.id));
+      alert('초대를 수락했습니다. 내 대시보드를 확인해보세요!');
     } catch (err) {
       console.error('handleAccept 처리 중 에러 발생:', err);
+      alert('초대 수락에 실패했습니다.');
+    } finally {
+      setIsProcessing(false);
     }
   };
 
-  const handleReject = async (invitation: InvitationsDto) => {
+  // 대시보드 초대 거절
+  const handleReject = async (invitation: invitationListDto) => {
+    setIsProcessing(true);
     try {
       await putInvitations(invitation.id, false);
-      setInvitations(prev => prev.filter(item => item.id !== invitation.id));
+      setInvitationList(prev => prev.filter(item => item.id !== invitation.id));
+      alert('초대를 거절했습니다.');
     } catch (err) {
       console.error('handleReject 처리 중 에러 발생:', err);
+      alert('초대 거절에 실패했습니다.');
+    } finally {
+      setIsProcessing(false);
     }
   };
 
@@ -73,7 +90,7 @@ export default function InvitedDashboard({
       <SearchForm onSearch={handleSearch} />
       <InviteHeader />
       {loading && <p>초대 목록을 불러오고 있습니다.</p>}
-      {error && <p>{error}</p>}
+      {error && <p>오류가 발생했습니다: {error}</p>}
       <ul className="flex flex-col gap-[20px]">
         {filteredResults.length === 0 ? (
           <li className="mt-12 flex flex-col items-center justify-center gap-2">
@@ -81,7 +98,7 @@ export default function InvitedDashboard({
             <ReturnButton buttonText="전체 목록 보기" onClick={handleReset} />
           </li>
         ) : (
-          filteredResults.map((invitation: InvitationsDto) => (
+          filteredResults.map((invitation: invitationListDto) => (
             <li
               key={invitation.id}
               className="flex w-full justify-around border-b border-gray-100 pb-[20px] text-center text-black-600"
@@ -89,13 +106,9 @@ export default function InvitedDashboard({
               <p>{invitation.dashboard.title}</p>
               <p>{invitation.inviter.nickname}</p>
               <p>
-                <button
-                  type="button"
-                  className="w-[84px] rounded-[4px] bg-violet px-4 py-2 text-white"
-                  onClick={() => handleAccept(invitation)}
-                >
+                <PrimaryButton onClick={() => handleAccept(invitation)}>
                   수락
-                </button>
+                </PrimaryButton>
                 <button
                   type="button"
                   className="border-1 ml-2 w-[84px] rounded-[4px] border border-gray-200 px-4 py-2 text-violet"
