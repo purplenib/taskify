eda6e5ee2481342ed0262c7eb38d0d5db910d11e
DESKTOP-THOA91P\영주 <breadjoo1014@naver.com>
2024-09-06 12:24:11 +0900

    TASK-38 [Refactor]: API 호출 최소화, 상태로 mydashboard 관리

--- src/core/contexts/MyDashboardProvider.tsx
+++ src/core/contexts/MyDashboardProvider.tsx
@@ -1,10 +1,5 @@
 'use Client';
 
-import {
-  INIT_DASHBOARDS_REQUEST,
-  INIT_DASHBOARDS_RESPONSE,
-  INIT_MYDASHBOARDS_CONTEXT,
-} from '@lib/constants/initialValue';
 import React, {
   createContext,
   useContext,
@@ -12,56 +7,77 @@ import React, {
   useState,
   useMemo,
 } from 'react';
+
+import getDashboards from '@core/api/getDashboards';
+import { INIT_MYDASHBOARDS_CONTEXT } from '@lib/constants/initialValue';
+import useApi from '@lib/hooks/useApi';
+
 import type {
-  DashboardDto,
+  DashboardApplicationServiceResponseDto,
   MyDashboardContextDto,
 } from '@core/dtos/DashboardsDto';
-import getDashboards from '@core/api/getDashboards';
-import useApi from '@lib/hooks/useApi';
 
 const MyDashboardContext = createContext<MyDashboardContextDto>(
   INIT_MYDASHBOARDS_CONTEXT
 );
 
 export const MyDashboardProvider = ({ children }) => {
-  const [myDashboards, setMyDashboards] = useState<DashboardDto[]>([]);
-  const [localDashboards, setLocalDashboards] = useState<DashboardDto[]>([]);
+  const [fetchedDashboards, setFetchedDashboards] = useState<
+    DashboardApplicationServiceResponseDto[]
+  >([]);
+  const [localDashboards, setLocalDashboards] = useState<
+    DashboardApplicationServiceResponseDto[]
+  >([]);
 
-  //  사용자의 대시보드 데이터 패칭
-  const { data, loading, error } = useApi(
-    getDashboards,
-    INIT_DASHBOARDS_REQUEST,
-    INIT_DASHBOARDS_RESPONSE
-  );
+  const { data, isLoading, error, callApi } = useApi('/dashboards', 'GET');
 
-  // 대시보드 데이터가 변경될 때마다 상태 업데이트
+  // 컴포넌트가 마운트될 때 API 호출
+  useEffect(() => {
+    const fetchData = async () => {
+      const config = {
+        params: {
+          navigationMethod: 'pagination',
+        },
+      };
+      await callApi(undefined, config);
+    };
+
+    fetchData();
+  }, [callApi]);
+
+  // 로컬 대시보드 추가
+  const addDashboard = (
+    newDashboard: DashboardApplicationServiceResponseDto
+  ) => {
+    setLocalDashboards(prev => [...prev, newDashboard]);
+  };
+
+  // 대시보드 데이터가 변경될 때마다 로컬 대시보드 상태 업데이트
   useEffect(() => {
     if (data && data.dashboards) {
-      setMyDashboards(data.dashboards);
+      setFetchedDashboards(data.dashboards);
+      setLocalDashboards(prev => [...prev, ...data.dashboards]);
     }
   }, [data]);
 
-  const addDashboard = (newDashboard: DashboardDto) => {
-    setLocalDashboards(prev => [...prev, newDashboard]); // 로컬 대시보드 추가
-  };
-
-  // 대시보드 데이터 재패칭
+  // 대시보드 데이터 재패칭 함수
   const fetchDashboards = async () => {
     const response = await getDashboards();
-    setMyDashboards(response.dashboards);
+    setFetchedDashboards(response.dashboards);
+    setLocalDashboards(response.dashboards);
   };
 
   // Context 값 메모이제이션
   const value = useMemo(
     () => ({
-      myDashboards,
+      fetchedDashboards,
       localDashboards,
-      loading,
+      loading: isLoading,
       error,
       addDashboard,
       fetchDashboards,
     }),
-    [myDashboards, localDashboards, loading, error]
+    [fetchDashboards, localDashboards, isLoading, error]
   );
 
   return (
