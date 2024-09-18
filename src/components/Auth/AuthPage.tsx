0765fae1664a2b4e7722136ddf70dd23cfe60e82
purplenib <breadjoo1014@naver.com>
2024-09-18 11:39:01 +0000

    main [Fix]: 로그인 시 버튼 활성화 분기 수정

--- src/components/Auth/AuthPage.tsx
+++ src/components/Auth/AuthPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import axios from 'axios';
@@ -35,9 +35,9 @@ export default function AuthPage({ mode }: AuthPageProps) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
     watch,
-  } = useForm<FormValues>({ mode: 'onBlur' });
+  } = useForm<FormValues>({ mode: 'onChange' });
 
   const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
@@ -46,9 +46,9 @@ export default function AuthPage({ mode }: AuthPageProps) {
   const [isModalVisible, setIsModalVisible] = useState(false); // 모달 상태
   const [modalMessage, setModalMessage] = useState(''); // 모달 메시지
 
-  const emailValue = watch('email');
+  // const emailValue = watch('email');
   const passwordValue = watch('password');
-  const passwordConfirmValue = watch('passwordConfirm');
+  // const passwordConfirmValue = watch('passwordConfirm');
 
   // 모달이 닫힐 때 리다이렉트를 처리하는 useEffect
   useEffect(() => {
@@ -125,27 +125,27 @@ export default function AuthPage({ mode }: AuthPageProps) {
     }
   };
 
-  const isFormValid = useMemo(() => {
-    if (mode === 'signup') {
-      return (
-        termsAccepted &&
-        !Object.keys(errors).length &&
-        passwordValue === passwordConfirmValue
-      );
-    }
-    return (
-      Boolean(emailValue) &&
-      Boolean(passwordValue) &&
-      !Object.keys(errors).length
-    );
-  }, [
-    emailValue,
-    passwordValue,
-    passwordConfirmValue,
-    errors,
-    termsAccepted,
-    mode,
-  ]);
+  // const isFormValid = useMemo(() => {
+  //  if (mode === 'signup') {
+  //    return (
+  //      termsAccepted &&
+  //      !Object.keys(errors).length &&
+  //      passwordValue === passwordConfirmValue
+  //    );
+  //  }
+  //  return (
+  //    Boolean(emailValue) &&
+  //    Boolean(passwordValue) &&
+  //    !Object.keys(errors).length
+  //  );
+  // }, [
+  //  emailValue,
+  //  passwordValue,
+  //  passwordConfirmValue,
+  //  errors,
+  //  termsAccepted,
+  //  mode,
+  // ]);
 
   return (
     <div className="flex min-h-screen items-center justify-center p-4">
@@ -254,9 +254,9 @@ export default function AuthPage({ mode }: AuthPageProps) {
           )}
           <button
             type="submit"
-            disabled={!isFormValid}
+            disabled={!isValid}
             className={`h-[50px] w-full rounded-[8px] px-4 py-2 font-semibold text-white ${
-              isFormValid
+              isValid
                 ? 'bg-indigo-600 hover:bg-indigo-700'
                 : 'cursor-not-allowed bg-gray-300'
             } focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
